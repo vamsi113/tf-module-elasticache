ca823f3c1fa1bc08dc5ece1dd7b9cbe6b772ad2d
vamsi113 <krishultimate5@gmail.com>
2023-06-17 21:10:33 -0400

    change

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "aws_elasticache_cluster" "main" {
   num_cache_nodes      = var.num_cache_nodes
   parameter_group_name = aws_elasticache_parameter_group.default.name
   engine_version       = var.engine_version
-  vpc_security_group_ids = [aws_security_group.sg.id]
+  security_group_ids = [aws_security_group.sg.id]
   subnet_group_name = aws_elasticache_subnet_group.default.name
 }
 
